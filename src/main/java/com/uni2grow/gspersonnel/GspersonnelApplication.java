d70d726161a6d023cf6cae392aa6df35d0e76eb8
lind7 <kakeulinda7@gmail.com>
2020-02-13 10:21:16 +0100

    Initial commit

---
+++ src/main/java/com/uni2grow/gspersonnel/GspersonnelApplication.java
@@ -0,0 +1,13 @@
+package com.uni2grow.gspersonnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GspersonnelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GspersonnelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uni2grow/gspersonnel/GspersonnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni2grow.gspersonnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GspersonnelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
